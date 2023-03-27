510b33adba22b5e9516cb54b8db81e7f76f54619
shvmsh20 <itsshivamsharma21@gmail.com>
2023-03-27 21:07:40 +0530

    added: src folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./Media/CSS/App.css";
+import Main from "../src/Components/main"
+
+function App() {
+  return (
+    <>
+      <div className='leftSideBox'></div>
+      <Main />
+    </>
+  );
+}
+
+export default App;
