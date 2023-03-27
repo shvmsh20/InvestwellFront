14272e1fb60630b4dbdfbda26fcd289ff3d6e8f0
shvmsh20 <itsshivamsharma21@gmail.com>
2023-03-27 12:39:08 +0530

    initial commit

---
+++ src/Components/errorPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <>
+      <div className="errorPage">
+        <h1>OOPS!</h1>
+        <h2>Something is not good!!</h2>
+      </div>
+    </>
+  );
+}
+
+export default ErrorPage;
